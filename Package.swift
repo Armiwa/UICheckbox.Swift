9616f73c173d41528eb0974a176ef3da598ad4c7
Armiwa <devarmiwa@gmail.com>
2020-07-23 17:10:12 +0900

    path 변경

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .target(
             name: "UICheckbox.Swift",
             dependencies: [],
-            path: "Sources"),
+            path: "UICheckbox"),
     ]
 )
